7770df1c3cf9328f73e85a271ce18d49b7b35eaa
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2019-07-18 12:25:28 +0300

    feat(core): add discover page with who are using ngx

---
+++ demo/src/app/common/discover/discover.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'discover',
+  templateUrl: './discover.component.html'
+})
+export class DiscoverComponent {
+  name = `Native Angular widgets for Bootstrap 3 and Bootstrap 4 - Discover more`;
+  src = 'https://github.com/valor-software/ngx-bootstrap';
+}
